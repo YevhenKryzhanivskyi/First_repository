5da17b5e1b3e2740d4c3e370de0d6f4cec918114
YevhenKryzhanivskyi <yevhenkrizha@gmail.com>
2025-10-09 18:22:39 +0300

    final HW4

--- goit-pycore-hw-04-01.py
+++ goit-pycore-hw-04-01.py
@@ -33,14 +33,28 @@ def normalize_phone_number(phone:str)->str:
 wright_number = normalize_phone_number("0932836370")
 print("Нормалізований номер телефону:", wright_number)
 
-def get_upcoming_birthday(users:dict, birthday:str)->str: #Завдання 4
-    try:
-        birth_date = datetime.strptime(birth_date, "%Y-%m-%d").date()
-        today = datetime.today().date()
-        current_year_birthday = today+7
-        if today==current_year_birthday:
-            current_year_birthday = current_year_birthday.replace(year=today.year + 1)
-        days_until_birthday = (current_year_birthday - today).days
-        return f"До наступного дня народження залишилось {days_until_birthday} днів."
-    except ValueError:
-        return "Невірний формат дати. Використовуйте 'YYYY-MM-DD'."
+from datetime import datetime, timedelta #Завдання 4
+def get_upcoming_birthdays(users:list)->list: 
+    today=datetime.today().date()
+    upcoming_birthdays=[]
+    for user in users:
+        name=user["name"]
+        birthday=datetime.strptime(user["birthday"], "%Y-%m-%d").date()
+        birthday_this_year=birthday.replace(year=today.year)
+        if birthday_this_year<today:
+            birthday_this_year=birthday_this_year.replace(year=today.year+1)
+        days_until_birthday=(birthday_this_year-today).days
+        if 0 <= days_until_birthday <= 7:
+          congratulation_date = birthday_this_year
+    if congratulation_date.weekday() == 5:  # субота
+        congratulation_date += timedelta(days=2)
+    elif congratulation_date.weekday() == 6:  # неділя
+        congratulation_date += timedelta(days=1)
+    upcoming_birthdays.append({
+        "name": name,
+        "congratulation_date": congratulation_date.strftime("%Y.%m.%d")
+    })
+    return upcoming_birthdays
+users = [{"name": "Марія", "birthday": "1992-10-13"},]
+result = get_upcoming_birthdays(users)
+print("result", result)
