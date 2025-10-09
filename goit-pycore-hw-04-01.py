945ff8ab303343a4c921de63a291580fabfa8e5a
YevhenKryzhanivskyi <yevhenkrizha@gmail.com>
2025-10-09 15:00:54 +0300

    HW 04-01. Функція get_days_from_today

---
+++ goit-pycore-hw-04-01.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+def get_days_from_today(date:str)->int:
+    try:
+        date = datetime.strptime(date, "%Y-%m-%d").date()
+        today = datetime.today().date()
+        delta = today - date
+        return delta.days 
+    except ValueError:
+        return "Невірний формат дати. Використовуйте 'YYYY-MM-DD'." 
+
+result = get_days_from_today("2024-10-09")
+print("result", result)
+# Форматування лише часу
