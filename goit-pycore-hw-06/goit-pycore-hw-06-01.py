c900043f0e0ba722eccec8f8c7c9f8890720caa8
YevhenKryzhanivskyi <yevhenkrizha@gmail.com>
2025-11-02 15:41:23 +0200

    HW6

---
+++ goit-pycore-hw-06/goit-pycore-hw-06-01.py
@@ -0,0 +1,70 @@
+from collections import UserDict
+
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+
+class Name(Field):
+    pass
+
+
+class Phone(Field):
+    def __init__(self, value):
+        self.validate(value)
+        super().__init__(value)
+
+    @staticmethod
+    def validate(value):
+        if not value.isdigit() or len(value) != 10:
+            raise ValueError("Телефонний номер повинен містити 10 цифр.")
+
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def add_phone(self, phone_number):
+        phone = Phone(phone_number)
+        self.phones.append(phone)
+
+    def remove_phone(self, phone_number):
+        for phone in self.phones:
+            if phone.value == phone_number:
+                self.phones.remove(phone)
+                return True
+        return False
+
+    def edit_phone(self, old_number, new_number):
+        for index, phone in enumerate(self.phones):
+            if phone.value == old_number:
+                self.phones[index] = Phone(new_number)
+                return True
+        return False
+
+    def find_phone(self, phone_number):
+        for phone in self.phones:
+            if phone.value == phone_number:
+                return phone
+        return None
+
+    def __str__(self):
+        phones_str = '; '.join(phone.value for phone in self.phones)
+        return f"Contact name: {self.name.value}, phones: {phones_str}"
+
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def find(self, name):
+        return self.data.get(name)
+
+    def delete(self, name):
+        if name in self.data:
+            del self.data[name]
